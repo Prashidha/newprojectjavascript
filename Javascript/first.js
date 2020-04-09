e25fafe3b9a6a680fa6be5e8ddc03307bdd2c3b0
38682824 <prashidha.developer@gmail.com>
2020-04-09 21:52:59 +1000

    commit after arithmetic operators

--- Javascript/first.js
+++ Javascript/first.js
@@ -4,3 +4,18 @@ general = 1;
 console.log(typeof general);
 general = "New";
 console.log(typeof general);
+general = 'New';
+console.log(typeof general);
+general = true;
+console.log(typeof general);
+general = null;
+console.log(typeof general);
+general = [5,6.7,8,9];
+console.log(typeof general);
+console.log(general.length);
+
+console.log(2+3);
+console.log("Hello " + "there");
+console.log(2+" Hi");
+
+prompt("Enter some value");
---
+++ Javascript/operators.js
@@ -0,0 +1,15 @@
+// Arithmetic operators
+
+console.log(3+2);
+var differnce = 2-1;
+console.log("The differnce is " +differnce);
+differnce++;
+console.log("the unary operators value is " +differnce);
+var product = 283*241;
+console.log("The product is " +product);
+var differnce = 2-1;
+console.log("The differnce is " +differnce);
+var div = 540/25 ;
+console.log("The Division is " +div);
+var differnce = 9%5;
+console.log("The differnce is " +differnce);
